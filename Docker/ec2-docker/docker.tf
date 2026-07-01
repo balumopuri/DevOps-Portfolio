f7e8d85b78fd7a7d17771c8040fa9da5f501c7db
balumopuri <balumopuri19@gmail.com>
2026-07-02 02:54:36 +0530

    protofolio updated

--- Docker/ec2-docker/docker.tf
+++ Docker/ec2-docker/docker.tf
@@ -26,6 +26,13 @@ resource "aws_security_group" "allow_tls" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+      ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 
   egress {
     from_port   = 0
