6a4d3192a8dde6108839d32c01fed84ba82f9051
balumopuri <balumopuri19@gmail.com>
2026-07-03 02:04:27 +0530

    ARG added

--- Docker/ec2-docker/docker.tf
+++ Docker/ec2-docker/docker.tf
@@ -1,16 +1,25 @@
 resource "aws_instance" "this" {
-  ami                    = "ami-0220d79f3f480ecf5" # This is our devops-practice AMI ID
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  ami                    = "ami-0220d79f3f480ecf5" # devops-practice AMI
   instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo dnf install -y dnf-plugins-core
+    sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+    sudo dnf install -y docker-ce docker-ce-cli containerd.io
+    sudo systemctl start docker
+    sudo systemctl enable docker
+  EOF
+
   tags = {
     Name    = "docker"
-    Purpose = "terraform-practice"
   }
 }
 
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls_df"
-  description = "Allow TLS inbound traffic and all outbound traffic"
+  description = "Allow SSH, HTTP, and 8080 inbound; all outbound"
 
   ingress {
     from_port   = 22
@@ -19,20 +28,19 @@ resource "aws_security_group" "allow_tls" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-    ingress {
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-      ingress {
+  ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
 
   egress {
     from_port   = 0
@@ -46,7 +54,6 @@ resource "aws_security_group" "allow_tls" {
   }
 }
 
-output "aws_instance" {
+output "aws_instance_public_ip" {
   value = aws_instance.this.public_ip
-  
 }
