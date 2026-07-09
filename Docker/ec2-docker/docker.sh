f12fd8e271d3867b59bbb07eb099ef5b2631eb98
balumopuri <balumopuri19@gmail.com>
2026-07-09 15:48:44 +0530

    Docker.sh added

--- Docker/ec2-docker/docker.sh
+++ Docker/ec2-docker/docker.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-ARCH=amd64
-PLATFORM=windows_$ARCH
 
-
+# ---------------------------------------
+# Disk / LVM expansion
+# ---------------------------------------
 growpart /dev/nvme0n1 4
 lvextend -l +50%FREE /dev/RootVG/rootVol
 lvextend -l +50%FREE /dev/RootVG/varVol
 xfs_growfs /
 xfs_growfs /var
 
+# ---------------------------------------
+# Docker installation
+# ---------------------------------------
 dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
@@ -16,26 +19,31 @@ systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
 
-
-#kubectl installation
+# ---------------------------------------
+# kubectl installation
+# ---------------------------------------
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.32.0/2024-12-20/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mv kubectl /usr/local/bin/kubectl
+kubectl version --client
 
-#eksctl
+# ---------------------------------------
+# eksctl installation
+# ---------------------------------------
+ARCH=amd64
+PLATFORM=linux_$ARCH     # was "windows_$ARCH" — fixed for a Linux server
 
+dnf install -y unzip      # unzip not present by default on RHEL/Amazon Linux
 
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.zip"
 
-
-# # (Optional) Verify checksum
+# (Optional) Verify checksum
 # curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
 
-unzip eksctl_$PLATFORM.zip -d $HOME/bin
-
+mkdir -p /tmp/eksctl-install
+unzip eksctl_$PLATFORM.zip -d /tmp/eksctl-install
 rm eksctl_$PLATFORM.zip
 
-
-chmod +x ./eksctl
-
-mv /tmp/eksctl /usr/local/bin
+chmod +x /tmp/eksctl-install/eksctl
+mv /tmp/eksctl-install/eksctl /usr/local/bin/eksctl
+eksctl version
