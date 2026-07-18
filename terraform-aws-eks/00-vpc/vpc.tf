7be77946b1b93df545af98a9277e51b42dd73018
balumopuri <balumopuri19@gmail.com>
2026-07-18 18:50:05 +0530

    K8 Updated

--- terraform-aws-eks/00-vpc/vpc.tf
+++ terraform-aws-eks/00-vpc/vpc.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source       = "../../Terraform-VPC/VPC module"
+  source       = "../VPC module"
   project_name = var.project_name
   environment  = var.environment
   common_tags  = var.common_tags
