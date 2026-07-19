fea3dc5b47c5097669512a547f9f29d95ba7b3ee
balumopuri <balumopuri19@gmail.com>
2026-07-19 18:10:15 +0530

    Updated

--- terraform-aws-eks/40-eks/provider.tf
+++ terraform-aws-eks/40-eks/provider.tf
@@ -1,22 +1,21 @@
 terraform {
   required_version = ">= 1.0"
+
   required_providers {
-    # Add your required providers here
-    # Example:
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.28.0"
+      version = "~> 5.95"
     }
   }
-  backend "s3"{
-    bucket = "terraform-module-expense"
-    key = "expense-app-eks"
-    region = "us-east-1"
+
+  backend "s3" {
+    bucket         = "terraform-module-expense"
+    key            = "expense-app-eks"
+    region         = "us-east-1"
     dynamodb_table = "bala-terraform-prd"
   }
 }
 
-# Configure the provider
 provider "aws" {
   region = "us-east-1"
 }
