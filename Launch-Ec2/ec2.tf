5f68ff11a58519e7a04f5444cc62efaa99b760eb
Bala Mopuri <balumopuri19@gmail.com>
2026-08-04 09:25:04 +0000

    launch pad added

--- Launch-Ec2/ec2.tf
+++ Launch-Ec2/ec2.tf
@@ -1,32 +1,34 @@
 resource "aws_instance" "this" {
-  ami                    = "ami-0220d79f3f480ecf5" # This is our devops-practice AMI ID
-  vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
+  ami                    = "ami-0220d79f3f480ecf5" # DevOps Practice AMI
   instance_type          = "t3.medium"
+  vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
 
   user_data = file("jenkins.sh")
+
+  # Increase root volume size
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3"
+  }
+
   tags = {
     Name = "Jenkins-Server"
   }
 }
 
-  # 20GB is not enough
-  root_block_device {
-    volume_size = 50  # Set root volume size to 50GB
-    volume_type = "gp3"  # Use gp3 for better performance (optional)
-  }
-}
-
 resource "aws_security_group" "allow_all_docker" {
   name        = "allow_all_docker"
-  description = "Allow TLS inbound traffic and all outbound traffic"
+  description = "Allow Jenkins and SSH access"
 
+  # SSH
   ingress {
-    from_port   = 0
-    to_port     = 0
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Jenkins
   ingress {
     from_port   = 8080
     to_port     = 8080
@@ -34,6 +36,7 @@ resource "aws_security_group" "allow_all_docker" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Allow all outbound traffic
   egress {
     from_port   = 0
     to_port     = 0
@@ -42,10 +45,10 @@ resource "aws_security_group" "allow_all_docker" {
   }
 
   tags = {
-    Name = "allow_tls"
+    Name = "allow_all_jenkins"
   }
 }
-  
+
 output "jenkins_ip" {
-  value       = aws_instance.this.public_ip
+  value = aws_instance.this.public_ip
 }
